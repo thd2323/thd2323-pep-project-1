6b310e9ab5497acd6520ba154eeea58f8cba8d03
thd2323 d <thd23@pitt.edu>
2024-10-12 20:54:03 +0000

    adw gggg aaaa

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,32 @@
+package DAO;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountDAO {
+
+    public List<Account> getAllAccounts(){
+        Connection connection = ConnectionUtil.getConnection();
+        List<Account> accounts = new ArrayList<>();
+        try {
+            //Write SQL logic here
+            String sql = "SELECT * FROM account";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()){
+                Account account = new Account(rs.getInt("account_id"), rs.getString("username"),
+                        rs.getString("password"));
+                accounts.add(account);
+            }
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return accounts;
+    }
+
+}
