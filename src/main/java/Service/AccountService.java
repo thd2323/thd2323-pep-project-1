6c133ef64dbe21aa338f6d1c5a9aa4590cc19f18
thd2323 d <thd23@pitt.edu>
2024-10-12 21:02:57 +0000

    fffff faf

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,5 +1,24 @@
 package Service;
 
+import Model.Account;
+import DAO.AccountDAO;
+
+import java.util.List;
+
 public class AccountService {
+    AccountDAO accountDAO;
+
+    public AccountService(){
+        accountDAO = new AccountDAO();
+    }
+
+    public AccountService(AccountDAO accountDAO){
+        this.accountDAO = accountDAO;
+    }
+    //get all accounts
+    public List<Account> getAllAccounts() {
+        return accountDAO.getAllAccounts();
+    }
+
 
 }
